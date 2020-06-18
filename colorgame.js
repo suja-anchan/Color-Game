b94046cb87eaed777bec515850dad22a6b611b3a
Suja <Suja.mbidri@gmail.com>
2020-06-18 23:13:42 +0530

    easy and hard mode

--- colorgame.js
+++ colorgame.js
@@ -1,16 +1,66 @@
-var colors=[ 
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)",
-]
+// var colors=[ 
+// "rgb(255, 0, 0)",
+// "rgb(255, 255, 0)",
+// "rgb(0, 255, 0)",
+// "rgb(0, 255, 255)",
+// "rgb(0, 0, 255)",
+// "rgb(255, 0, 255)",
+// ]
+
+
+var colors = generateRandomColors(6);
 var squares=document.querySelectorAll(".square");
-var pickedcolor=colors[3];
+var pickedcolor=pickcolor();
 var colordisp=document.querySelector("#colordisp");
 var msgdisp=document.querySelector("#dispMsg");
+
+var h1=document.querySelector("h1");
+var resetb= document.querySelector("#reset");
+var easyb=document.querySelector("#easy");
+var hardb=document.querySelector("#hard");
+
+easyb.addEventListener("click",function(){
+hardb.classList.remove("selected");
+easyb.classList.add("selected");
+colors=generateRandomColors(3);
+pickedcolor=pickcolor();
 colordisp.textContent=pickedcolor;
+for(var i=0;i<squares.length;i++)
+{
+if(colors[i]){
+	squares[i].style.background=colors[i];
+}
+else
+{
+	squares[i].style.display="none";
+}
+}
+
+});
+
+hardb.addEventListener("click",function(){
+easyb.classList.remove("selected");
+hardb.classList.add("selected");
+colors=generateRandomColors(6);
+pickedcolor=pickcolor();
+colordisp.textContent=pickedcolor;
+for(var i=0;i<squares.length;i++)
+{
+	squares[i].style.display="block";
+}
+
+});
+
+resetb.addEventListener("click",function(){
+	colors=generateRandomColors(6);
+	pickedcolor= pickcolor();
+	colordisp.textContent=pickedcolor;
+	for(var i=0;i<squares.length;i++)
+	  squares[i].style.backgroundColor=colors[i];
+	
+	h1.style.background="#232323";
+});
+
 
 for(var i=0;i<squares.length;i++){
 	//add initial colors to squares
@@ -22,7 +72,10 @@ for(var i=0;i<squares.length;i++){
 		 var clickd=this.style.backgroundColor;
 		if(clickd === pickedcolor){
 
-			alert("correct");
+			msgdisp.textContent="correct";
+			resetb.textContent="Play Again?";
+			changeclr(clickd);
+			h1.style.backgroundColor= clickd;
 		}
 		else
 		{
@@ -35,4 +88,41 @@ for(var i=0;i<squares.length;i++){
 	});
 }
 
+function changeclr(color){
+	for (var i=0; i<colors.length;i++){
+		squares[i].style.backgroundColor=color;
+	}
+}
 
+function pickcolor(){
+	var random=Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num)
+{
+	//make an array
+	var arr=[];
+
+
+	
+	for ( var i=0;i<num;i++){
+		//add num random colors to array
+		arr.push(randomColor());
+	}
+
+	//return array
+	return arr;
+}
+
+function randomColor(){
+	//pick a red from 0-255
+	var r=Math.floor(Math.random() * 256);
+
+	//pick a green from 0-255
+	var g=Math.floor(Math.random() * 256);
+	//pick a blue from 0-255
+	var b=Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}	
